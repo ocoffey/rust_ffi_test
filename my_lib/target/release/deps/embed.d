f1d436900397581d96710b2efb3aff9f11e35c7e
Olivia Coffey <ocoffey@protonmail.com>
2020-01-28 14:49:06 -0500

    leaving in dll for now, will delete when the project is complete

---
+++ my_lib/target/release/deps/embed.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/livvy/Documents/Github/rust_ffi_test/my_lib/target/release/deps/libembed.so: src/lib.rs
+
+/mnt/c/Users/livvy/Documents/Github/rust_ffi_test/my_lib/target/release/deps/embed.d: src/lib.rs
+
+src/lib.rs:
---
+++ my_lib/target/release/libembed.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/livvy/Documents/Github/rust_ffi_test/my_lib/target/release/libembed.so: /mnt/c/Users/livvy/Documents/Github/rust_ffi_test/my_lib/src/lib.rs
